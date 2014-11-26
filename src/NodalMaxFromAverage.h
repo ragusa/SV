f3180a6ef328064941bc610baed5fdaffd452b21
Jean C. Ragusa <jean.ragusa@tamu.edu>
2014-11-26 01:43:59 -0600

    ps from marco

---
+++ src/NodalMaxFromAverage.h
@@ -0,0 +1,31 @@
+#ifndef NODALMAXFROMAVERAGE_H
+#define NODALMAXFROMAVERAGE_H
+
+#include "NodalVariablePostprocessor.h"
+
+class NodalMaxFromAverage;
+
+template<>
+InputParameters validParams<NodalMaxFromAverage>();
+
+/**
+ *
+ */
+class NodalMaxFromAverage : public NodalVariablePostprocessor
+{
+public:
+  NodalMaxFromAverage(const std::string & name, InputParameters parameters);
+  virtual ~NodalMaxFromAverage();
+
+  virtual void initialize();
+  virtual void execute();
+  virtual Real getValue();
+  virtual void threadJoin(const UserObject & y);
+
+protected:
+  Real _value;
+  std::string _average_name;
+  const PostprocessorValue & _average;
+};
+
+#endif /* NODALMAXFROMAVERAGE_H_ */
