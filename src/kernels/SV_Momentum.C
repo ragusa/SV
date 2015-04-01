98a6d04ba7c9f4f6da363441251efced256f69c6
Jean Ragusa <jean.ragusa@tamu.edu>
2015-04-01 18:40:20 -0500

    cannot have requiredparam and supply a default value at the same time
    cannot have requiredparam and supply a default value at the same time

--- src/kernels/SV_Momentum.C
+++ src/kernels/SV_Momentum.C
@@ -30,9 +30,9 @@ InputParameters validParams<SV_Momentum>()
   // Coupled aux variables
     params.addCoupledVar("B", "bathymetry data");// jcr how about addRequiredCoupledVar instead
   // Constants and parameters
-    params.addParam<Real>("gravity", 9.81, "Gravity magnitude");
+    params.addRequiredParam<Real>("gravity", "Gravity magnitude");
     params.addRequiredParam<UserObjectName>("eos", "Equation of state");
-    params.addRequiredParam<unsigned int>("component", 0, "component of the momentum equation to compute (0,1)->(x,y)");
+    params.addRequiredParam<unsigned int>("component", "component of the momentum equation to compute (0,1)->(x,y)");
     
   return params;
 }
