1118446031a63c3e19ff423c57cdc0a6797a422a
Jean Ragusa <jean.ragusa@tamu.edu>
2015-04-27 18:28:22 -0500

    missed a coupled of explicit files in kernel

---
+++ include/kernels/SV_ArtificialViscFlux_Explicit.h
@@ -0,0 +1,56 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef SV_ARTIFICIALVISCFLUX_EXPLICIT_H
+#define SV_ARTIFICIALVISCFLUX_EXPLICIT_H
+
+#include "Kernel.h"
+
+// Forward Declarations
+class SV_ArtificialViscFlux_Expl;
+
+template<>
+InputParameters validParams<SV_ArtificialViscFlux_Expl>();
+
+class SV_ArtificialViscFlux_Expl : public Kernel
+{
+public:
+
+  SV_ArtificialViscFlux_Expl(const std::string & name,
+                             InputParameters parameters);
+
+protected:
+
+  virtual Real computeQpResidual();
+
+  virtual Real computeQpJacobian();
+
+  virtual Real computeQpOffDiagJacobian(unsigned int _jvar);
+    
+private:
+  // Equations types
+  enum EquationType
+  {
+    CONTINUITY = 0,
+    X_MOMENTUM = 1,
+    Y_MOMENTUM = 2
+  };
+  // Equations type
+  MooseEnum _equ_type;
+
+  // Material property: viscosity coefficient.
+  MaterialProperty<Real> & _kappa;
+};
+
+#endif // SV_ARTIFICIALVISCFLUX_EXPLICIT_H
--- src/kernels/SV_ArtificialViscFlux_Explicit.C
+++ src/kernels/SV_ArtificialViscFlux_Explicit.C
@@ -12,13 +12,13 @@
 /*            See COPYRIGHT for full restrictions               */
 /****************************************************************/
 
-#include "SV_ArtificialViscFlux.h"
+#include "SV_ArtificialViscFlux_Explicit.h"
 /**
 This function computes the dissipative terms for all of the equations. It is 
 dimension-agnostic and equation-agnostic.
  */
 template<>
-InputParameters validParams<SV_ArtificialViscFlux>()
+InputParameters validParams<SV_ArtificialViscFlux_Expl>()
 {
   InputParameters params = validParams<Kernel>();
   // Equation name:
@@ -27,8 +27,8 @@ InputParameters validParams<SV_ArtificialViscFlux>()
   return params;
 }
 
-SV_ArtificialViscFlux::SV_ArtificialViscFlux(const std::string & name,
-                                             InputParameters parameters) :
+SV_ArtificialViscFlux_Expl::SV_ArtificialViscFlux_Expl(const std::string & name,
+                                                       InputParameters parameters) :
   Kernel(name, parameters),
   // Declare equation types
   _equ_type("CONTINUITY X_MOMENTUM Y_MOMENTUM INVALID", getParam<std::string>("equation_name")),
@@ -37,7 +37,7 @@ SV_ArtificialViscFlux::SV_ArtificialViscFlux(const std::string & name,
 {
 }
 
-Real SV_ArtificialViscFlux::computeQpResidual()
+Real SV_ArtificialViscFlux_Expl::computeQpResidual()
 {
   // Determine if cell is on boundary or not:
   if (_mesh.isBoundaryNode(_current_elem->node(_i))==true) 
@@ -49,27 +49,27 @@ Real SV_ArtificialViscFlux::computeQpResidual()
     switch (_equ_type)  // jcr all the same, single line then?
     {
     case CONTINUITY:
-       return _kappa[_qp]*_grad_u[_qp]*_grad_test[_i][_qp];
-       break;
+      return _kappa[_qp]*_grad_u[_qp]*_grad_test[_i][_qp];
+      break;
     case X_MOMENTUM:
-       return _kappa[_qp]*_grad_u[_qp]*_grad_test[_i][_qp];
-       break;
+      return _kappa[_qp]*_grad_u[_qp]*_grad_test[_i][_qp];
+      break;
     case Y_MOMENTUM:
-       return _kappa[_qp]*_grad_u[_qp]*_grad_test[_i][_qp];
-       break;
+      return _kappa[_qp]*_grad_u[_qp]*_grad_test[_i][_qp];
+      break;
     default:
       mooseError("INVALID equation name.");
     }
   }
 }
 
-Real SV_ArtificialViscFlux::computeQpJacobian()
+Real SV_ArtificialViscFlux_Expl::computeQpJacobian()
 {
   // We assumed that the all of the above regularization can be approximated by the parabolic regularization:
   return _kappa[_qp]*_grad_phi[_j][_qp]*_grad_test[_i][_qp];
 }
 
-Real SV_ArtificialViscFlux::computeQpOffDiagJacobian( unsigned int _jvar)
+Real SV_ArtificialViscFlux_Expl::computeQpOffDiagJacobian( unsigned int _jvar)
 {
   // With above assumption, there is no contribution from the dissipative terms to the off diagonal terms of the jacobian matrix.
   return 0.*_jvar;
