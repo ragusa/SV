8b77f42a24d17bc51affee7bcf0d2ad3afa8aff6
Jean Ragusa <jean.ragusa@tamu.edu>
2015-05-18 18:12:39 -0500

    adding rectangular hump as bathymetry function

---
+++ include/functions/Bathymetry1D_rect.h
@@ -0,0 +1,43 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef BATHYMETRY1D_RECT_H
+#define BATHYMETRY1D_RECT_H
+
+#include "Function.h"
+#include "FunctionInterface.h" // jcr what is this for?
+
+class Bathymetry1D_rect;
+
+template<>
+InputParameters validParams<Bathymetry1D_rect>();
+
+class Bathymetry1D_rect : public Function
+{
+public:
+  Bathymetry1D_rect(const std::string & name, InputParameters parameters);
+
+  virtual Real value(Real t, const Point & p);
+
+  virtual RealVectorValue gradient(Real t, const Point & p);
+
+protected:
+    
+    // domain length
+    Real _length;
+    Real _height;
+
+};
+
+#endif //BATHYMETRY1D_RECT_H
---
+++ src/functions/Bathymetry1D_rect.C
@@ -0,0 +1,48 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "Bathymetry1D_rect.h"
+
+template<>
+InputParameters validParams<Bathymetry1D_rect>()
+{
+  InputParameters params = validParams<Function>();
+  params.addParam<Real>("domain_length", 1500., "Length of the domain" );
+  params.addParam<Real>("step_height", 8., "Height of the step" );
+  return params;
+}
+
+Bathymetry1D_rect::Bathymetry1D_rect(const std::string & name, InputParameters parameters) :
+  Function(name, parameters),
+  _length(getParam<Real>("domain_length")),
+  _height(getParam<Real>("step_height"))
+{
+  if (_height <= 0.)
+    mooseError("'" << this->name() << "' : height of step cannot be <=0.");
+}
+
+Real
+Bathymetry1D_rect::value(Real /*t*/, const Point & p)
+{
+  if ( std::abs(p(0)-_length/2) < _length/_height )
+    return _height;
+  else 
+    return 0.;
+}
+
+RealVectorValue 
+Bathymetry1D_rect::gradient(Real /*t*/, const Point & p)
+{
+  return 0.;
+}
