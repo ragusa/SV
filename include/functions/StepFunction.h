977cbfc4e3c2aca68bbc0202e4f214f581b8ea0d
Jean C. Ragusa <jean.ragusa@tamu.edu>
2015-05-20 22:52:46 -0500

    adding a generic step function

---
+++ include/functions/StepFunction.h
@@ -0,0 +1,46 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef STEPFUNCTION_H
+#define STEPFUNCTION_H
+
+#include "Function.h"
+#include "FunctionInterface.h" // jcr what is this for?
+
+class StepFunction;
+
+template<>
+InputParameters validParams<StepFunction>();
+
+class StepFunction : public Function
+{
+public:
+  StepFunction(const std::string & name, InputParameters parameters);
+
+  virtual Real value(Real t, const Point & p);
+
+  virtual RealVectorValue gradient(Real t, const Point & p);
+
+protected:
+    
+    // values for a step function
+    Real _value_before_step;
+    Real _value_after_step;
+    Real _x0; // location of the step
+    Real _y0; // location of the step
+    Real _z0; // location of the step
+
+};
+
+#endif //STEPFUNCTION_H
---
+++ src/functions/StepFunction.C
@@ -0,0 +1,68 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "StepFunction.h"
+
+template<>
+InputParameters validParams<StepFunction>()
+{
+  InputParameters params = validParams<Function>();
+  params.addParam<Real>("value_before_step"    , 0., "Value before the step" );
+  params.addParam<Real>("value_after_step"     , 1., "Value after the step");
+  params.addParam<Real>("x_step",0.5, "x-location of step");
+  params.addParam<Real>("y_step",0.5, "y-location of step");
+  params.addParam<Real>("z_step",0.5, "z-location of step");
+  return params;
+}
+
+StepFunction::StepFunction(const std::string & name, InputParameters parameters) :
+  Function(name, parameters),
+  _value_before_step(getParam<Real>("value_before_step")),
+  _value_after_step(getParam<Real>("value_after_step")),
+  _x0(getParam<Real>("x_step")),
+  _y0(getParam<Real>("y_step")),
+  _z0(getParam<Real>("z_step"))
+{}
+
+Real
+StepFunction::value(Real /*t*/, const Point & p)
+{
+  switch( _mesh.dimension() ) 
+  {
+  case 1:
+    if ( p(0)<_x0 )
+      return _value_before_step;
+    else 
+      return _value_after_step;
+    break;
+  case 2:
+    if ( p(0)<_x0 && p(1)<_y0 )
+      return _value_before_step;
+    else 
+      return _value_after_step;
+    break;
+  case 3:
+    if ( p(0)<_x0 && p(1)<_y0 && p(2)<_z0 )
+      return _value_before_step;
+    else 
+      return _value_after_step;
+    break;
+  } 
+}
+
+RealVectorValue 
+StepFunction::gradient(Real /*t*/, const Point & p)
+{
+    return _grad_zero;
+}
