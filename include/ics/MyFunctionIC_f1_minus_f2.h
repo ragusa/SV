ad0f1ba03bee193303fc39f00b2f6056e820c99d
Jean C. Ragusa <jean.ragusa@tamu.edu>
2015-05-20 23:01:55 -0500

    adding a new IC: f1-f2

---
+++ include/ics/MyFunctionIC_f1_minus_f2.h
@@ -0,0 +1,58 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef MYFUNCTIONIC_H
+#define MYFUNCTIONIC_H
+
+#include "FunctionIC.h"
+
+//Forward Declarations
+class MyFunctionIC_f1_minus_f2;
+class Function;
+
+template<>
+InputParameters validParams<FunctionIC>();
+
+/**
+ * Defines a boundary condition that forces the value to be a user specified
+ * function at the boundary.
+ */
+class MyFunctionIC_f1_minus_f2 : public FunctionIC
+{
+public:
+  MyFunctionIC_f1_minus_f2(const std::string & name, InputParameters parameters);
+
+protected:
+  /**
+   * Evaluate the function at the current quadrature point and timestep.
+   */
+  Real f();
+
+  /**
+   * The value of the variable at a point.
+   */
+  virtual Real value(const Point &p);
+
+  /**
+   * The value of the gradient at a point.
+   */
+  virtual RealGradient gradient(const Point &p);
+
+  Function & _func1;
+  Function & _func2;
+
+
+};
+
+#endif // MYFUNCTIONIC_H
---
+++ src/ics/MyFunctionIC_f1_minus_f2.C
@@ -0,0 +1,45 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "MyFunctionIC_f1_minus_f2.h"
+#include "Function.h"
+
+template<>
+InputParameters validParams<MyFunctionIC_f1_minus_f2>()
+{
+  InputParameters params = validParams<FunctionIC>();
+  params.addRequiredParam<FunctionName>("function1", "The 1st function needed to define the initial condition.");
+  params.addRequiredParam<FunctionName>("function2", "The 2nd function needed to define the initial condition.");
+  
+  return params;
+}
+
+MyFunctionIC_f1_minus_f2::MyFunctionIC_f1_minus_f2(const std::string & name, InputParameters parameters) :
+    FunctionIC(name, parameters),
+    _func1(getFunction("function1")),
+    _func1(getFunction("function2"))
+{
+}
+
+Real
+MyFunctionIC_f1_minus_f2::value(const Point & p)
+{
+  return _func1.value(_t, p) - _func2.value(_t, p);
+}
+
+RealGradient
+MyFunctionIC_f1_minus_f2::gradient(const Point & p)
+{
+  return _func1.gradient(_t, p) * _func2.gradient(_t, p);
+}
